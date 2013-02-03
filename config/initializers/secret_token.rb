e9bc96569d911a4ed0590cddf320e30b7e8b7a19
Ivan Rostovsky <ir@irostovsky.local>
2013-02-03 16:43:18 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codility::Application.config.secret_token = '8935b8635add3d74c5a139e3f79466057651a858839d6a2d4717b9b417f96f24026b587b6b350951ac19bbd6b6f3460fc5c29a6cf76753077609d2e57f7cebb6'
